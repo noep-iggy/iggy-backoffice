6a842857fc67eeb3e8d5ffa5d8f4f00b09d23d94
Noé <noe.philippe@bob-le-developpeur.com>
2024-01-08 13:46:21 +0100

    🐛 reduce image build

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
+/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires, no-undef */
+
 const { i18n } = require('./next-i18next.config');
-const runtimeCaching = require('next-pwa/cache');
-const withPreconstruct = require('@preconstruct/next');
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  // swcMinify: true,
-});
+const settings = {
+  i18n,
+  staticPageGenerationTimeout: 20000,
+  output: 'standalone',
+  // https://github.com/vercel/next.js/issues/48748#issuecomment-1578374105
+  modularizeImports: {
+    '@heroicons/react/outline/?(((\\w*)?/?)*)': {
+      transform: '@heroicons/react/outline/{{ matches.[1] }}/{{member}}',
+    },
+    '@heroicons/react/solid/?(((\\w*)?/?)*)': {
+      transform: '@heroicons/react/solid/{{ matches.[1] }}/{{member}}',
+    },
+  },
+};
 
-module.exports = withPreconstruct(
-  withPWA({
-    i18n,
-    staticPageGenerationTimeout: 20000,
-  })
-);
+module.exports = settings;
